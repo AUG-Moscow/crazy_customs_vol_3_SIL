945a9d0f842aeac49fc7d1176daad00b7e8022fa
Morzhabyss <78736578+Morzhabyss@users.noreply.github.com>
2021-02-08 17:54:18 +0300

    Create request70.js

---
+++ request70.js
@@ -0,0 +1,2 @@
+AJS.$("#content > div > div.aui-page-panel-inner.cv-page-body-inner > div > div > form > div:nth-child(5) > label > span").hide()
+AJS.$("#content > div > div.aui-page-panel-inner.cv-page-body-inner > div > div > form > div:nth-child(6) > label > span").hide()
